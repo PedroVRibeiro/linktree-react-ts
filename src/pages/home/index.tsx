61c43ecec1e0575aed8dbe270315df1273940c95
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2025-02-05 14:50:36 -0300

    create base homepage structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,19 @@
 export function Home() {
   return (
-    <div>
-      <h1>Página Home</h1>
+    <div className='flex flex-col w-full py-4 items-center justify-center'>
+      <h1 className='md:text-4xl text-3xl font-bold text-white mt-20'>Pedro Ribeiro</h1>
+      <span className='text-gray-50 mb-5 mt-3'>Veja meus links</span>
+
+      <main className='flex flex-col w-11/12 max-w-xl text-center'>
+        <section className='bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105'>
+          <a href="">
+            <p className='text-base md:text-lg'>
+              LinkedIn
+            </p>
+          </a>
+        </section>
+      </main>
+
     </div>
   )
 }

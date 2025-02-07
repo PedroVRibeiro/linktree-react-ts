e348bd0a14b41772e26007f32f518a8c3b146520
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2025-02-07 14:54:09 -0300

    set up basic form logic

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
+import { FormEvent, useState } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    console.log({ email: email, password: password });
+  }
+
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -13,8 +23,23 @@ export function Login() {
         </h1>
       </Link>
 
-      <form className="w-full max-w-x1 flex flex-col px-2">
-        <Input placeholder="Digite seu Email:" />
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-x1 flex flex-col px-2">
+        <Input
+          placeholder="Digite seu Email:"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="Digite sua senha:"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <button
           type="submit"
           className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
